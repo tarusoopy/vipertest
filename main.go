0a147e6a153ebcb11ae0c4c6dcc6203f04628518
h.nakano <h.nakano@variosecure.net>
2020-08-21 21:39:49 +0900

    色々お試し中。まだ読み込めていない

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/fsnotify/fsnotify"
 	flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"os"
@@ -34,8 +33,6 @@ type DbInfo struct {
 	DbName string `config:"dbname"`
 }
 
-var config Config
-
 func main() {
 	var configfile string
 	flag.StringVarP(
@@ -48,51 +45,70 @@ func main() {
 
 	e := readConfig(configfile)
 	if e != nil {
+		fmt.Printf("%s\n", e)
 		os.Exit(-1)
 	}
 }
 
 func readConfig(configfile string) error {
+	var vpninfo = VpnInfo{}
+	var dbinfo = DbInfo{}
+	var pcregister = PcRegister{}
+	var config = Config{
+		vpninfo:    vpninfo,
+		dbinfo:     dbinfo,
+		pcregister: pcregister,
+	}
+
 	// コンフィグ読み込み
 	viper.SetConfigFile(configfile)
-	viper.AddConfigPath("/etc/variosecure")
+	fmt.Printf("configfile: %s\n", configfile)
+	//viper.AddConfigPath("/etc/variosecure")
+	//viper.SetConfigType("json")
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Errorf("設定ファイル読み込みエラー: %s \n", err)
 		return err
 	}
-	viper.WatchConfig()
-	viper.OnConfigChange(func(e fsnotify.Event) {
-		if err := viper.ReadInConfig(); err != nil {
-			fmt.Errorf("設定ファイル読み込みエラー: %s \n", err)
-		}
-	})
+	fmt.Printf("cust_id: %s\n", viper.GetString("cust_id"))
 
 	// 値読み込み
-	if err := viper.Unmarshal(&config); err != nil {
-		return err
-	}
-
 	/*
-		vpninfo.CustId = viper.GetString("cust_id")
-		vpninfo.VpnNetwork = viper.GetString("vpn_network")
-		vpninfo.VpnAddress = viper.GetString("vpn_address")
-
-		dbinfo.DbUser = viper.GetString("dbuser")
-		dbinfo.DbPass = viper.GetString("dbpass")
-		dbinfo.DbHost = viper.GetString("dbhost")
-		dbinfo.DbName = viper.GetString("dbname")
-
-		pcregister.NewPcEndpoint = viper.GetString("newpc_endpoint")
-		pcregister.IdMatchEdpoint = viper.GetString("idmatch_endpoint")
-		pcregister.IpRegisterEndpoint = viper.GetString("ipregister_endpoint")
+		if err := viper.Unmarshal(&config); err != nil {
+			return err
+		}
 	*/
 
-	printConfig()
+	config.vpninfo.CustId = viper.GetString("cust_id")
+	config.vpninfo.VpnNetwork = viper.GetString("vpn_network")
+	config.vpninfo.VpnAddress = viper.GetString("vpn_address")
+
+	config.dbinfo.DbUser = viper.GetString("dbuser")
+	config.dbinfo.DbPass = viper.GetString("dbpass")
+	config.dbinfo.DbHost = viper.GetString("dbhost")
+	config.dbinfo.DbName = viper.GetString("dbname")
+
+	config.pcregister.NewPcEndpoint = viper.GetString("newpc_endpoint")
+	config.pcregister.IdMatchEdpoint = viper.GetString("idmatch_endpoint")
+	config.pcregister.IpRegisterEndpoint = viper.GetString("ipregister_endpoint")
+
+	fmt.Printf("cust_id: %s\n", config.vpninfo.CustId)
+	fmt.Printf("vpn_network: %s\n", config.vpninfo.VpnNetwork)
+	fmt.Printf("vpn_address: %s\n", config.vpninfo.VpnAddress)
+	fmt.Printf("dbuser: %s\n", config.dbinfo.DbUser)
+	fmt.Printf("dbpass: %s\n", config.dbinfo.DbPass)
+	fmt.Printf("dbhost: %s\n", config.dbinfo.DbHost)
+	fmt.Printf("dbname: %s\n", config.dbinfo.DbName)
+	fmt.Printf("newpc_endpoint: %s\n", config.pcregister.NewPcEndpoint)
+	fmt.Printf("idmatch_endpoint: %s\n", config.pcregister.IdMatchEdpoint)
+	fmt.Printf("ipregister_endpoint: %s\n", config.pcregister.IpRegisterEndpoint)
+	fmt.Printf("port: %s\n", config.pcregister.Port)
+	//printConfig(&config)
 
 	return nil
 }
 
-func printConfig() {
+/*
+func printConfig(config *Config) {
 	fmt.Printf("cust_id: %s\n", config.vpninfo.CustId)
 	fmt.Printf("vpn_network: %s\n", config.vpninfo.VpnNetwork)
 	fmt.Printf("vpn_address: %s\n", config.vpninfo.VpnAddress)
@@ -105,3 +121,4 @@ func printConfig() {
 	fmt.Printf("ipregister_endpoint: %s\n", config.pcregister.IpRegisterEndpoint)
 	fmt.Printf("port: %s\n", config.pcregister.Port)
 }
+*/
